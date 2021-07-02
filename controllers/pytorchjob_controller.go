361f52c5ea142b2c385b52f2fb21ddca13798a36
Wang Zhang <lucaswzhang@tencent.com>
2021-07-02 08:06:54 +0800

    temp

--- controllers/pytorchjob_controller.go
+++ controllers/pytorchjob_controller.go
@@ -18,6 +18,10 @@ package controllers
 
 import (
 	"context"
+	commonv1 "github.com/kubeflow/common/pkg/apis/common/v1"
+	"github.com/kubeflow/common/pkg/util"
+	apiequality "k8s.io/apimachinery/pkg/api/equality"
+	"k8s.io/client-go/tools/cache"
 
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
@@ -30,8 +34,13 @@ import (
 // PyTorchJobReconciler reconciles a PyTorchJob object
 type PyTorchJobReconciler struct {
 	client.Client
+	commonv1.ControllerInterface
 	Scheme *runtime.Scheme
 }
+
+var (
+	KeyFunc = cache.DeletionHandlingMetaNamespaceKeyFunc
+)
 
 //+kubebuilder:rbac:groups=kubeflow.org,resources=pytorchjobs,verbs=get;list;watch;create;update;patch;delete
 //+kubebuilder:rbac:groups=kubeflow.org,resources=pytorchjobs/status,verbs=get;update;patch
@@ -48,8 +57,58 @@ type PyTorchJobReconciler struct {
 // - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.8.3/pkg/reconcile
 func (r *PyTorchJobReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
 	_ = log.FromContext(ctx)
+	logger := util.LoggerForKey(req.NamespacedName.String())
 
-	// your logic here
+	var job pytorchv1.PyTorchJob
+	if err := r.Get(ctx, req.NamespacedName, &job); err != nil {
+		logger.Error(err, "unable to fetch PyTorchJob")
+		return ctrl.Result{}, client.IgnoreNotFound(err)
+	}
+
+	oldStatus := job.Status.DeepCopy()
+	pods, err := r.GetPodsForJob(&job)
+	if err != nil {
+		logger.Warnf("getPodsForPyTorchJob error %v", err)
+		return ctrl.Result{}, err
+	}
+
+	services, err := r.GetServicesForJob(job)
+	if err != nil {
+		logger.Warnf("getServicesForPyTorchJob error %v", err)
+		return ctrl.Result{}, err
+	}
+
+	// If the PyTorchJob is terminated, delete all pods and services.
+	if isSucceeded(job.Status) || isFailed(job.Status) {
+		if err := r.deletePodsAndServices(job, pods, services); err != nil {
+			return err
+		}
+
+		if err := r.cleanupPyTorchJob(job); err != nil {
+			return err
+		}
+
+		if r.Config.EnableGangScheduling {
+			if err := r.DeletePodGroup(job); err != nil {
+				return err
+			}
+		}
+
+		// At this point the pods may have been deleted, so if the job succeeded, we need to manually set the replica status.
+		// If any replicas are still Active, set their status to succeeded.
+		if isSucceeded(job.Status) {
+			for rtype := range job.Status.ReplicaStatuses {
+				job.Status.ReplicaStatuses[rtype].Succeeded += job.Status.ReplicaStatuses[rtype].Active
+				job.Status.ReplicaStatuses[rtype].Active = 0
+			}
+		}
+		if !apiequality.Semantic.DeepEqual(*oldStatus, job.Status) {
+			return r.updateStatusHandler(job)
+		}
+		return nil
+	}
+
+	r.
 
 	return ctrl.Result{}, nil
 }
